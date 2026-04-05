7f8a5cf10e8934856e154a52b4e752a5e870b91b
Garima Joshi <connectwithgarima1310@gmail.com>
2026-04-05 19:23:20 +0530

    Completed Frontend Section

--- pixels-reactjs/client/src/pages/YtPreview.tsx
+++ pixels-reactjs/client/src/pages/YtPreview.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import { yt_html } from "../assest/assets";
+import { useSearchParams } from "react-router-dom";
 const YtPreview = () => {
-  return (
-    <div>YtPreview</div>
-  )
-}
+  const [searchParams] = useSearchParams();
+  const thumbnail_url = searchParams.get("thumbnail_url");
+  const title = searchParams.get("thumbnail_url");
 
-export default YtPreview
+  const newHtml = yt_html
+    .replace("%%THUMBNAIL_URL%%", thumbnail_url!)
+    .replace("%%TITLE%%", title!);
+  return (
+    <div className="fixed inset-0 z-100 bg-black">
+      <iframe
+        srcDoc={newHtml}
+        width="100%"
+        height="100%"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
+
+export default YtPreview;
